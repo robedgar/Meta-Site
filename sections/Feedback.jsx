ce560171356b3146fdfb72024d1831c80a251cc7
robedgar <116982166+robedgar@users.noreply.github.com>
2022-11-29 13:09:18 +0000

    Update Feedback.jsx

--- sections/Feedback.jsx
+++ sections/Feedback.jsx
@@ -19,6 +19,30 @@ const Feedback = () => (
         className=" flex-[0.5] lg:max-w-[370px] flex justify-end flex-col gradient-05 sm:p-8 p-4 rounded-[32px] border-[1px] border-[#6a6a6a] relative"
       >
         <div className="feedback-gradient" />
+        <div>
+          <h4 className="font-bold sm:text-[32px] text-[22px] sm:leading-[40px] leading-[36px] text-white">
+            Samantha
+          </h4>
+          <p className="mt-[8px] font-normal sm:text[18px] text-[12px] sm:leading[22px] leading-[16px] text-white">
+            Founder | Metaversus
+          </p>
+        </div>
+        <p className="mt-[24px] font-normal sm:text[24px] text-[18px] sm:leading[45px] leading-[39px] text-white">
+          “With the development of today's technology,
+          metaverse is very useful for today's work,
+          or can be called web 3.0. by using metaverse you can use it as anything”
+        </p>
+      </motion.div>
+
+      <motion.div
+        varients={fadeIn('left', 'tween', 0.2, 1)}
+        className="relative flex-1 flex justify-center items-center"
+      >
+        <img
+          src="/planet-09.png"
+          alt="planet-09"
+          className="w-full"
+        />
       </motion.div>
     </motion.div>
   </section>
